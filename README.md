829a226bc977141141b5eb2977166f13f5aef1e8
mrsufgi <sufgis@gmail.com>
2022-03-02 12:51:57 +0200

    solution

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# question_answer
+# How to use?
+
+Make sure you have Node 16 installed
+
+1. install deps: `npm i`
+2. run `npm start "FILE_NAME" "ENTRY_NODE" "RELATION_1" "RELATION_2" ...`
+
+`FILE_NAME` - CVS filename containing dataset or format "source", "target", "edge"
+`ENTRY_NODE` - The node you want you question to start asking on, ie: "national recording registry"
+"RELATION_X" - A zero or many relations we want to follow (don't forget to put in quotes!)
+
+For example: `npm start "s_t_e_wiki_sent_2 - s_t_e_wiki_sent_2.csv" "cameras" "is" "planned"`
+For answering questions such as: "Who supervised the thing that was added to the national recording registry?"
+
+We could run: `npm start "s_t_e_wiki_sent_2 - s_t_e_wiki_sent_2.csv" "national recording registry" "added to" "supervised"`
+
+# About my solution
+
+The solution uses a simple backtracking (with unoptimized recursion in js) to find all possible paths of a "graph"
+built based on the CVS implemented using a dictionary. The keys are relations and values are lists of nodes connected by this relation.
+
+A proper graph could have given us better results as finding "relations" would be in O(1) and not O(N) (In my code I use .filter() to find possible paths)
---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+const path = require("path");
+const csv = require("fast-csv");
+const { argv } = require("process");
+
+function buildGraph(file) {
+  return new Promise((res, rej) => {
+    const graph = {};
+    fs.createReadStream(path.resolve(__dirname, file))
+      .pipe(csv.parse({ headers: true }))
+      .on("error", (error) => {
+        // error reading csv
+        rej(error);
+      })
+      .on("data", (row) => {
+        rel = graph[row.edge];
+        pair = { source: row.source, target: row.target };
+        if (!rel) {
+          // first relation find of type "edge"
+          graph[row.edge] = [pair];
+        } else {
+          // relations can have more than one pair
+          rel.push(pair);
+        }
+      })
+      .on("end", () => res(graph));
+  });
+}
+
+function askQuestion(graph, entry, relations = []) {
+  if (!relations.length) {
+    // we got to the root, if we got here, this means we reached the source
+    // therefore, this is the answer :)
+    return entry;
+  }
+  const currNode = entry;
+  const cloneRelations = relations.slice();
+  const currRel = cloneRelations.shift();
+  const optionalNodes = graph[currRel];
+  if (!optionalNodes) {
+    return;
+  }
+  // note: its possible to have a relation with the same target but different source, we must follow
+  // all permutations and see if this leads us to a single solution
+  const optionalSources = optionalNodes.filter(
+    (node) => node.target === currNode
+  );
+
+  for (const { source } of optionalSources) {
+    const node = askQuestion(graph, source, cloneRelations);
+    if (node) {
+      // if the recursion found a node, this means it contains a valid path.
+      return node;
+    }
+
+    // no nodes were found, this means that either the path was a dead end, and no edge exists.
+    continue;
+  }
+}
+
+async function ask(file, entry, relations) {
+  const graph = await buildGraph(file);
+  const answer = askQuestion(graph, entry, relations);
+
+  // only printing since no top-level await can print it directly from main.
+  if (!answer) {
+    console.log(
+      "unfortunatly we can't answer your questions with this dataset :("
+    );
+
+    return;
+  }
+
+  console.log("answer:", answer);
+  return answer;
+}
+
+// Note: needs top-level await support.
+ask(argv[2], argv[3], argv.slice(4));
